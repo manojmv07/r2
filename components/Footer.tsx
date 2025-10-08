ce7cc7249a8d54ec0fdc3388b7761f73cb515217
manojmv07 <manojmv047@gmail.com>
2025-10-08 22:09:39 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
         <footer className="w-full text-center py-6 px-4 z-20 relative">
             <div className="text-xs text-brand-subtle">
                 <p>© 2025 Prism. All rights reserved.</p>
-                <p className="mt-1">Built by Mithun, Damodar, Kaifulla, Ranjith</p>
+                <p className="mt-1">Built by Manoj, Mithun</p>
             </div>
         </footer>
     );
